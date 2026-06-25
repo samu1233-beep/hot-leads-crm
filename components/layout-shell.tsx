178015fe930a4e3f608d321a2a5caa26b3784e79
samu1233-beep <samu1233@gmail.com>
2026-06-25 15:13:56 +0300

    Update layout-shell.tsx

--- components/layout-shell.tsx
+++ components/layout-shell.tsx
@@ -6,13 +6,7 @@ export function LayoutShell({ title, subtitle, children }: { title: string; subt
       <div className="header">
         <div>
           <div className="brandWrap">
-            <div className="logoBox">
-              <img src="/upek_logo.jpg" alt="Upek logo" style={{ width: '100%', height: '100%', objectFit: 'contain' }} />
-            </div>
-            <div>
-              <div className="title">{title}</div>
-              <div className="subtitle">{subtitle}</div>
-            </div>
+            <img src="/upek_logo.jpg" alt="Upek logo" style={{ height: '80px', objectFit: 'contain' }} />
           </div>
         </div>
       </div>
